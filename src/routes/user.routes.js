79db2f7fa47ccc467d6d8e73db9d983d3fdc5b60
Snehashis Kundu <snehashiskundu17@gmail.com>
2025-04-14 17:47:39 +0530

    Enhance user routes by adding additional endpoints for user profile management and updating functionality

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {loginUser, logoutuser, registerUser,refreshaccesstoken} from "../controllers/user.controller.js";
+import {loginUser, logoutuser, registerUser,refreshaccesstoken, changePassword, getUserProfile, updateAccountDetails, updateUserAvatar, updateCoverImage, getuserChannelProfile, getwatchhistory} from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer_mid.js";
 import {verifyJWT} from "../middlewares/auth_id.js"
 const router= Router()
@@ -19,7 +19,13 @@ router.route("/register").post(
 
 router.route("/login").post(loginUser);
 
-router.route("/logout").post(verifyJWT,logoutuser)
-router.route("/refresh-token").post(refreshaccesstoken)
-
+router.route("/logout").post(verifyJWT,logoutuser);
+router.route("/refresh-token").post(refreshaccesstoken);
+router.route("/change-password").post(verifyJWT,changePassword);
+router.route("/current-user").get(verifyJWT,getUserProfile);
+router.route("/update-profile").patch(verifyJWT,updateAccountDetails);
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar);
+router.route("/cover-image").patch(verifyJWT,upload.single("/coverImage"),updateCoverImage);
+router.route("/c/:username").get(verifyJWT,getuserChannelProfile);
+router.route("/history").get(verifyJWT,getwatchhistory);
 export default router;
